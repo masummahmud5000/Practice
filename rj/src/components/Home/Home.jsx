0e6720ca4910e5870aedd3cdf125e578cef7979b
Masum Mahmud <mdmasumpresent@gmail.com>
2025-12-21 16:10:14 +0600

    async await add

--- rj/src/components/Home/Home.jsx
+++ rj/src/components/Home/Home.jsx
@@ -6,44 +6,56 @@ const Home = () => {
     const [info, setInfo] = useState([])
 
     useEffect(() => {
-        fetch('http://127.0.0.1:8000/main/api/')
-        .then(res => res.json())
-        .then(d => setInfo(d))
+        const LoadData = async () => {
+            try{
+                const url = await fetch('http://127.0.0.1:8000/main/api/');
+                const jsonConvert = await url.json();
+                setInfo(jsonConvert)
+            } catch (error){
+                console.log('not data fount',error)
+            }
+        }
+        LoadData();
     }, [])
     ////////////////////////////////////
-    const submitHandle = (e) => {
+    const submitHandle = async(e) => {
         // e.preventDefault()
 
         let yourName = e.target.yourName.value
         let yourAge = e.target.yourAge.value
         let yourNumber = e.target.yourNumber.value
         
-        fetch('http://127.0.0.1:8000/main/api/', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                name: yourName,
-                age: yourAge,
-                phoneNumber: yourNumber
-            }),
-        })
-        .then(res => res.json())
-        .then(d => console.log(d))
+        try{
+            const response = await fetch('http://127.0.0.1:8000/main/api/', {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({
+                    name: yourName,
+                    age: yourAge,
+                    phoneNumber: yourNumber,
+                }),
+            });
+            const data = await response.json();
+            console.log(data);
+        } catch (error){
+            console.log(error)
+        }
     };
 
     // Delete List 
-    const deleteUser = (id) => {
-        // console.log(id)
-
-        if (window.confirm('You are sure Click Item Delete!')){
-                fetch(`http://127.0.0.1:8000/main/api/${id}/`, {
-                method: 'DELETE'
-            });
-            window.location.reload();
+    const deleteUser = async(id) => {
+        if (window.confirm('your sure Delete this Data!')){
+            try{
+                const response = await fetch(`http://127.0.0.1:8000/main/api/${id}/`, {
+                    method: 'DELETE',
+                })
+                window.location.reload();
+                response();
+            }catch(error){
+                console.log(error)
+            }
         }else{
-            alert('Your Item is not Deleted!')
+            alert('your Data is not Deleted!')
         }
     };
     
