913abe019792119a387c1db18aac1421a1bcd489
Masum Mahmud <mdmasumpresent@gmail.com>
2025-12-21 14:25:52 +0600

    get post delete and desing

--- dj/main/urls.py
+++ dj/main/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . views import mainApi
 
 urlpatterns=[
-    path('api/', mainApi.as_view())
+    path('api/', mainApi.as_view()),
+    path('api/<int:pk>/', mainApi.as_view())
 ]
--- dj/main/views.py
+++ dj/main/views.py
@@ -8,7 +8,7 @@ from rest_framework import status
 # Create your views here.
 
 class mainApi(APIView):
-    def get(self, request):
+    def get(self, request, pk=None):
         queySet = Server.objects.all()
         serializer = seria(queySet, many=True)
         return Response(serializer.data)
@@ -19,3 +19,8 @@ class mainApi(APIView):
             serializer.save()
             return Response(serializer.errors, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        user = Server.objects.get(id=pk)
+        user.delete()
+        return Response('User Deleted Successfull !')
